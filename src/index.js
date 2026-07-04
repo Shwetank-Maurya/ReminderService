4590785098db75e8ccbd8ba6d0c9621a68074e83
Shwetank-Maurya <sd3564086@gmail.com>
2026-07-04 12:56:18 +0530

    added the notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const cron = require('node-cron');
+
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+//const { sendBasicEmail } = require('./services/email-service');
 
 const setupAndStartServer = () => {
     const app = express ();
@@ -12,12 +14,16 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () =>{
         console.log(`Server started at PORT: ${PORT}.`);
-        sendBasicEmail(
-            'support@admin.com',
-            '',
-            'This is a testing mail',
-            'Hey, how are you, I hope you like support'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     '',
+        //     'This is a testing mail',
+        //     'Hey, how are you, I hope you like support'
+        // );
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('Runnning the task every two minutes');
+        });
     });
 }
 
